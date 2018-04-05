3dc23558fdde8ba10242a5c3e39901b9507696ec
shamantyagi <daphnis.shaman@gmail.com>
2018-04-05 23:19:21 +0530

    provision for possible moves

--- js/main-game.js
+++ js/main-game.js
@@ -22,6 +22,8 @@ $(document).ready(function(){
 var chance=1;
 var chance_color = ["","circle-black","circle-white","circle-black"];
 var player_color = ["","black","white"];
+var possible_chance_color = ["","circle-black-light","circle-white-light","circle-black-light"];
+var game_over=0;
 var total_flips = 0; 
 var game_board = [
 					[0,0,0,0,0,0,0,0,0,0],
@@ -36,9 +38,12 @@ var game_board = [
 					[0,0,0,0,0,0,0,0,0,0],
 				];
 
+var total_possible_flips=0;
+
 // 2 = white's chance | 1 = black's chance | black is always first
 function make_move(i,j)
 {
+
 	var error_msg="";
 	if(game_board[i][j]==0)
 	{
@@ -73,7 +78,9 @@ function make_move(i,j)
 				document.getElementById("block-"+i+"-"+j).classList.remove("circle-white");						
 			}	
 			display_gameboard();
-		}	
+		}
+		total_possible_chances();
+	
 	}
 	else
 	{
@@ -180,9 +187,163 @@ function count_score()
 	document.getElementById('player-1-score').innerHTML=score_black;
 	document.getElementById('player-2-score').innerHTML=score_white;
 
-	if(score_black==0 || score_white == 0)
-		document.getElementById('info-area').innerHTML = 'GAME OVER !!!';
+	if(score_black==0 || score_white == 0 || game_over==1)
+	{
+		if(game_over==1 && score_black+score_white<64 && chance==1)
+		{
+			game_over=2;
+			chance=2;
+			document.getElementById('info-area').innerHTML = 'No possible move for BLACK<br/>white moves again!!';
+		}	
+		else if(game_over==1 && score_black+score_white<64 && chance==2)
+		{
+			game_over=2;
+			chance=1;
+			document.getElementById('info-area').innerHTML = 'No possible move for BLACK<br/>white moves again!!';
+		}	
+		else if(score_black>score_white)
+			document.getElementById('info-area').innerHTML = 'GAME OVER !!!<br/>BLACK wins!!';
+		else if(score_black<score_white)
+			document.getElementById('info-area').innerHTML = 'GAME OVER !!!<br/>WHITE wins!!';
+		else
+			document.getElementById('info-area').innerHTML = 'GAME OVER !!!<br/>match DRAW!!';
+
+	}
+	if(game_over==-1)
+		document.getElementById('info-area').innerHTML = 'GAME OVER !!!<br/>No possible move!!';
+
+
 
 //	console.log(s);
 }
 
+function total_possible_chances()
+{
+	var game_possible =0 ;
+	for(var i=1;i<=8;i++)
+	{
+		for(var j=1;j<=8;j++)
+		{
+
+				document.getElementById("block-"+i+"-"+j).classList.remove('circle-black-light');
+				document.getElementById("block-"+i+"-"+j).classList.remove('circle-white-light');
+			if(game_board[i][j]==0)
+			{
+				if(possible_chances(i,j))
+				{
+					game_possible++;
+				}
+
+			}
+					
+//			console.log('BOARD CHECK AT '+i+'-'+j+' total_possible_flips VAL = '+total_possible_flips);
+		}
+	}
+	if(game_possible==0)
+	{
+		if(game_over!=2)
+		game_over=1;
+		else if(game_over==2) game_over=-1;			
+	}
+
+}
+
+function possible_chances(x,y)
+{
+//console.log('checking @ '+x+'-'+y)
+
+	total_possible_flips=0;
+//	console.log('INSIDE LOOP AT '+x+'-'+y+' total_possible_flips VAL = '+total_possible_flips);
+
+	//UP
+	possible_flip_direction(x-1,y,-1,0);
+	if(total_possible_flips>0)
+	{
+		console.log('possible to move on'+x+'-'+y);
+				document.getElementById("block-"+x+"-"+y).classList.toggle(possible_chance_color[chance]);
+		return true;
+	}	
+
+	//DOWN
+	possible_flip_direction(x+1,y,1,0);
+	if(total_possible_flips>0)
+	{
+		console.log('possible to move on'+x+'-'+y)
+				document.getElementById("block-"+x+"-"+y).classList.toggle(possible_chance_color[chance]);
+		return true;
+	}	
+
+	//RIGHT
+	possible_flip_direction(x,y+1,0,1);
+	if(total_possible_flips>0)
+	{
+		console.log('possible to move on'+x+'-'+y)
+				document.getElementById("block-"+x+"-"+y).classList.toggle(possible_chance_color[chance]);
+		return true;
+	}	
+
+	//LEFT
+	possible_flip_direction(x,y-1,0,-1);
+	if(total_possible_flips>0)
+	{
+		console.log('possible to move on'+x+'-'+y)
+				document.getElementById("block-"+x+"-"+y).classList.toggle(possible_chance_color[chance]);
+		return true;
+	}	
+
+	//UP RIGHT
+	possible_flip_direction(x-1,y+1,-1,1);
+	if(total_possible_flips>0)
+	{
+		console.log('possible to move on'+x+'-'+y)
+				document.getElementById("block-"+x+"-"+y).classList.toggle(possible_chance_color[chance]);
+		return true;
+	}	
+
+	//UP LEFT
+	possible_flip_direction(x-1,y-1,-1,-1);
+	if(total_possible_flips>0)
+	{
+		console.log('possible to move on'+x+'-'+y)
+				document.getElementById("block-"+x+"-"+y).classList.toggle(possible_chance_color[chance]);
+		return true;
+	}	
+
+	//DOWN RIGHT
+	possible_flip_direction(x+1,y+1,1,1);
+	if(total_possible_flips>0)
+	{
+		console.log('possible to move on'+x+'-'+y)
+				document.getElementById("block-"+x+"-"+y).classList.toggle(possible_chance_color[chance]);
+		return true;
+	}	
+
+	//DOWN LEFT
+	possible_flip_direction(x+1,y-1,1,-1);
+	if(total_possible_flips>0)
+	{
+		console.log('possible to move on'+x+'-'+y)
+				document.getElementById("block-"+x+"-"+y).classList.toggle(possible_chance_color[chance]);
+		return true;
+	}	
+
+		return false;
+}
+
+function possible_flip_direction(x,y,i,j)
+{
+//	console.log('checking '+x+","+y+" = " + game_board[x][y]);
+	if(game_board[x][y]==chance)
+		return true;
+
+	if(game_board[x][y]==0)
+		return false;
+
+	if(possible_flip_direction(x+i,y+j,i,j))
+	{
+		total_possible_flips++;
+		return true;
+	}	
+
+	return false;
+}
